541313a6386a182343f9b2867c5fc04493e683e6
IlyaKLSHN <91084577+IlyaKLSHN@users.noreply.github.com>
2023-06-20 14:43:33 +0300

    4 files modified, 2 deleted

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,46 +1,63 @@
 import React, { useState, useEffect } from 'react';
 
-import twitterIcon from '../twitter.svg';
-import tumblrIcon from '../tumblr.svg';
-
 const Quotes = () => {
   const [quote, setQuote] = useState('');
   const [author, setAuthor] = useState('');
 
   useEffect(() => {
-    getQuote()
+    getQuote();
   }, []);
 
   const getQuote = () => {
     let url = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
     fetch(url)
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         let dataQuotes = data.quotes;
         let randomNum = Math.floor(Math.random() * dataQuotes.length);
         let randomQuote = dataQuotes[randomNum];
 
         setQuote(randomQuote.quote);
         setAuthor(randomQuote.author);
-      })
-  }
+      });
+  };
 
   const handleClick = () => {
     getQuote();
+  };
+
+  const [isDark, setDark] = useState(false);
+
+  function handleDarkMode() {
+    setDark(!isDark);
   }
+
+  React.useEffect(() => {
+    if (isDark) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [isDark]);
 
   return (
     <div id="quote-box">
-      <div id="text"><p>{quote}</p></div>
-      <div id="author"><p>{author}</p></div>
+      <button onClick={handleDarkMode} id="dark-mode-switch">
+        Switch Dark/Light mode
+      </button>
 
-      <div id="buttons">
-        <div className="social-media">
-        </div>
-        <button onClick={handleClick} id="new-quote">Новая цитата</button>
+      <div id="text">
+        <p>{quote}</p>
       </div>
+      <div id="author">
+        <p>{author}</p>
+      </div>
+
+      <button onClick={handleClick} id="new-quote">
+        New quote
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Quotes;
